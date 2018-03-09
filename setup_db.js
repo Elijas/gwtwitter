a74ba12fa4510551528cefb2cc65c8722b12e5fe
Elijas <master.elijas@gmail.com>
2018-03-09 14:37:51 +0200

    Add "setup initial pg tables" script

---
+++ setup_db.js
@@ -0,0 +1,13 @@
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL;
+
+const client = new pg.Client(connectionString);
+client.connect();
+const query = client.query(
+    'CREATE TABLE properties (\n' +
+    '    id SERIAL PRIMARY KEY,\n' +
+    '    token1 TEXT,\n' +
+    '    token2 TEXT,\n' +
+    ');');
+
+query.on('end', () => { client.end(); });
